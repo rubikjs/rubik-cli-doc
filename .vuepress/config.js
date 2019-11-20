e172379a3a9e0ed10d5d3057c6f9a0a9532a500a
Fancy <fancyboynet@gmail.com>
2019-11-20 16:08:30 +0800

    config

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  base: '/rubik-cli-doc/',
+  title: 'Rubik Cli',
+  description: 'One command, one structure',
+  themeConfig: {
+    logo: '/rubik.png',
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# rubik-cli-doc
